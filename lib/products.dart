10778c94828f0a52a0bb89493f1fd2863b7d8911
mfcarneiro <matheus.felipe123@gmail.com>
2019-05-23 09:06:01 -0300

    Section 5 - List and conditionals
    **Overall:**
    
    - Implemented a `ListView` with basic and long items
    - Conditionals related to returning a Widget
    
    **Performance:**
    
    - Performance tips for long lists:
     - Add the `.builder()` constructor, using `itemBuilder` and `itemCount`
    
    **Good Practices:**
    
    - Good practices about splitting the code into `Widgets` to provide mutch better reading of the content that was being showned
    - The `build` method should never returns null, return insted a `Container()` empty.

--- lib/products.dart
+++ lib/products.dart
@@ -5,27 +5,83 @@ import 'package:flutter/material.dart';
 
 class Products extends StatelessWidget {
   //* The `final` keyword it is the same convention to not mutate the data
-  final List<String> productsItem;
+  final List<String> products;
 
   //* Set up the constructor to propagate the data comming from outside
-  Products(this.productsItem);
+  Products([this.products = const []]);
+
+  Widget _productListItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        //* --> <Widget> It's a generic type on dart (In this case, A generic array type)
+        children: <Widget>[
+          //* --> On Dart, When use a dot notation, it means an constuctor, accessing the wanted feature
+          Image.asset(
+            'assets/food.jpg',
+          ),
+          Text(products[index])
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductList() {
+    Widget productCards;
+
+    if (products.length > 0) {
+      productCards = ListView.builder(
+          itemBuilder: _productListItem, itemCount: products.length);
+    } else {
+      productCards = Center(
+        child: Text('No Products found'),
+      );
+    }
+    //! Never return NULL, the build method does not accept null
+    //! Instead, return a `Container()` empty (This is valid code, don't impact performance or anything either)
+    //? E.g:
+    //? } else {
+    //?   productCards = Container();
+    //? }
+    //? return productCards;
+    //?
+    return productCards;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: productsItem
-            .map((element) => Card(
-                  child: Column(
-                    //* --> <Widget> It's a generic type on dart (In this case, A generic array type)
-                    children: <Widget>[
-                      //* --> On Dart, When use a dot notation, it means an constuctor, accessing the wanted feature
-                      Image.asset(
-                        'assets/food.jpg',
-                      ),
-                      Text(element)
-                    ],
-                  ),
-                ))
-            .toList());
+    return _buildProductList();
   }
 }
+
+//* Ternary example
+//   @override
+//   Widget build(BuildContext context) {
+//     return productsItem.length > 0
+//         /? ListView.builder(itemBuilder: _productListItem, itemCount: products.length)
+//         : Center(
+//             child: Text('No Products found'),
+//           );
+//   }
+// }
+
+//* old way
+//   @override
+//   Widget build(BuildContext context) {
+
+//     return ListView( //* For performance purpuses, it will be called ListView.builder()
+//         children: productsItem //* List view no use children property
+//             .map((element) => Card(
+//                   child: Column(
+//                     //* --> <Widget> It's a generic type on dart (In this case, A generic array type)
+//                     children: <Widget>[
+//                       //* --> On Dart, When use a dot notation, it means an constuctor, accessing the wanted feature
+//                       Image.asset(
+//                         'assets/food.jpg',
+//                       ),
+//                       Text(element)
+//                     ],
+//                   ),
+//                 ))
+//             .toList());
+//   }
+// }
