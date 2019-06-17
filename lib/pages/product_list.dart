e9c8bcdee270ab4b9cab2eb43c521ea35ea1c4f8
mfcarneiro <matheus.felipe123@gmai.com>
2019-06-16 21:44:05 -0300

    Section 9 | Forms
    Changed the TextField Widget for TextFormField.
    
    New TextFormField features:
    - Added some validation on `validator` property
    - The `onChange()` is no longer needed, it was replaced with `onSave()`
     - The `setState()` uses reactivity to refresh for new added values, since this feature is not needed in this point of time, the `onSave()` will hold the given information to be submited
     - When submission is triggered, the `_formKey.currentState.save()` is used to store the field values mentioned above
     - This bound between field and submission method is provied by `GlobalKey`, that suits as a bridge, connecting the **current state** of the Widget, monitoring the input fields
    - A propety called `initValue` from `TextFormField()` receives a initial value that will show when the component is rendered
     - Useful when have a need to update some data already created
    
    __p.s: Some improvements are made, splitting the code and refactor something that was not covered on this current section__.

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -1,8 +1,40 @@
 import 'package:flutter/material.dart';
 
+// Widget
+import './product_edit.dart';
+
 class ProductListPage extends StatelessWidget {
+  final Function updateProduct;
+  final List<Map<String, dynamic>> productList;
+
+  ProductListPage(this.productList, this.updateProduct);
+
+  _buildProductEditor(BuildContext context, int index) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      return ProductEditPage(
+        product: productList[index],
+        updateProduct: updateProduct,
+        productIndex: index,
+      );
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+              leading: Image.asset('assets/food.jpg'),
+              title: Text(productList[index]['title']),
+              trailing: IconButton(
+                icon: Icon(Icons.mode_edit),
+                onPressed: () => _buildProductEditor(context, index),
+              ));
+        },
+        itemCount: productList.length,
+      ),
+    );
   }
 }
